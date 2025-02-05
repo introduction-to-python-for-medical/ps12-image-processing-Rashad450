9d570b0a17e767476eea53522116f2dc922d796c
Rashad450 <rashadkh29@gmail.com>
2025-02-06 01:08:05 +0200

    Update main.py

--- main.py
+++ main.py
@@ -6,4 +6,4 @@ lena=load_image('lena.jpg')
 clean=median(lena, ball(3))
 binary= l_edge > 100
 edge_image = Image.fromarry(np.uint8(binary* 225))
-edge_image.save('my_edge.png')
+edge_image.save('my_edges.png')
