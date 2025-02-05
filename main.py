31dca08e8b93cab717cbcc85a483b812a9a0c57e
Rashad450 <rashadkh29@gmail.com>
2025-02-06 01:07:35 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-from image_utilsimport load_image,edge_detection
+from image_utils import load_image,edge_detection
 from skimage.filters import median 
 from skimage.morphology import ball 
 from PIL import image 
 lena=load_image('lena.jpg')
-clean= l_edge > 100
+clean=median(lena, ball(3))
+binary= l_edge > 100
 edge_image = Image.fromarry(np.uint8(binary* 225))
 edge_image.save('my_edge.png')
